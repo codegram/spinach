25e9abf8d66768dec0659f2fce563f5ae5eb8395
Josep Jaume <josepjaume@gmail.com>
2011-09-24 20:18:50 +0200

    Initializing inception engines
    YO DAWG! I herd you like integration testing frameworks, so I wrote a
    BDD framework that's tested with the BDD framework I wrote!

---
+++ features/steps/feature_name_guessing.rb
@@ -0,0 +1,12 @@
+class FeatureNameGuessing < Spinach::Feature
+  feature "Feature name guessing"
+
+  Given 'I am writing a feature called "Feature name guessing"' do
+  end
+  And 'I write a class named "FeatureNameGuessing"' do
+  end
+  When 'I run spinach'do
+  end
+  Then 'I want "FeatureNameGuessing" class to be used to run it' do
+  end
+end
