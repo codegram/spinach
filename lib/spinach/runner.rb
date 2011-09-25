e46d4f5954e263b50e21425b2f33a8b672d0cfaf
Josep Jaume <josepjaume@gmail.com>
2011-09-25 17:27:21 +0200

    Add rest of the docs

--- lib/spinach/runner.rb
+++ lib/spinach/runner.rb
@@ -1,18 +1,27 @@
 module Spinach
+  # Spinach's runner gets the parsed data from the feature and performs the
+  # actual calls to the feature classes.
+  #
   class Runner
+    # Initializes the runner with a parsed feature
+    # @param [Hash] data
+    #   the parsed feature data
+    #
     def initialize(data)
       @feature_name = data['name']
       @scenarios = data['elements']
     end
 
+    # Returns the feature class for the provided feature data
+    # @return [Spinach::Feature] feature
+    #   this runner's feature
+    #
     def feature
       @feature ||= Spinach.find_feature(@feature_name)
     end
 
-    def scenarios
-      @scenarios
-    end
-
+    # Runs this runner and outputs the results in a colorful manner.
+    #
     def run
       step_count = 0
       reports = []
@@ -37,5 +46,12 @@ module Spinach
         puts report.backtrace.reverse.join("\n")
       end
     end
+
+  private
+
+    def scenarios
+      @scenarios
+    end
+
   end
 end
