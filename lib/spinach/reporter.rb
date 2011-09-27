6a389e26d62e10dfd94e568eba8f6a943c820e44
Josep Jaume <josepjaume@gmail.com>
2011-09-27 18:50:06 +0200

    Improve reporter output

--- lib/spinach/reporter.rb
+++ lib/spinach/reporter.rb
@@ -52,7 +52,7 @@ module Spinach
       # Prints the scenario name to the standard ouput
       #
       def scenario(name)
-        puts "  Scenario: #{name}".white
+        puts "\n  Scenario: #{name}".white
       end
 
       # Prints the step name to the standard output. If failed, it puts an
@@ -63,11 +63,11 @@ module Spinach
         connector = words.shift
         phrase = words.join(" ")
         if result == :success
-          puts "  ✔  #{connector} #{phrase}".green
+          puts "    ✔  #{connector} #{phrase}".green
         elsif result == :failure
-          puts "  ✘  #{connector} #{phrase}".red
+          puts "    ✘  #{connector} #{phrase}".red
         elsif result == :skip
-          puts "  ~  #{connector} #{phrase}".yellow
+          puts "    ~  #{connector} #{phrase}".cyan
         end
       end
 
