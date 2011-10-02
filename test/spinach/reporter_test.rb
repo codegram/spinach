76b81db82329358560bd5ff16dc7daa79cd51e1a
Josep Jaume <josepjaume@gmail.com>
2011-10-02 19:18:14 +0200

    Even more refactoring

--- test/spinach/reporter_test.rb
+++ test/spinach/reporter_test.rb
@@ -2,29 +2,4 @@
 require_relative '../test_helper'
 
 describe Spinach::Reporter do
-  describe 'abstract methods' do
-    let(:reporter) { Spinach::Reporter.new }
-
-    %w{feature scenario}.each do |abstract_method|
-      describe '#{abstract_method}' do
-        it 'raises an error' do
-          proc {
-            reporter.send(abstract_method, 'arbitrary name')
-          }.must_raise RuntimeError
-        end
-      end
-    end
-
-    describe '#step' do
-      it 'raises an error' do
-        proc { @reporter.step(stub, :success) }.must_raise RuntimeError
-      end
-    end
-
-    describe '#end' do
-      it 'raises an error' do
-        proc { reporter.end(true) }.must_raise RuntimeError
-      end
-    end
-  end
 end
