4fd101e193465c5abc87e469430c99813efc7290
Josep Jaume <josepjaume@gmail.com>
2011-10-03 20:45:23 +0200

    Refactor reporter

--- lib/spinach/reporter.rb
+++ lib/spinach/reporter.rb
@@ -23,87 +23,41 @@ module Spinach
 
     attr_reader :undefined_steps, :failed_steps, :error_steps
 
-    # Receives this hook when a feature is invoked
-    # @param [Hash] data
-    #   the feature data
-    #
-    def feature(data)
-      raise 'You need to define the `feature` method in your reporter!'
-    end
-
-    # Receives this hook when a scenario is invoked
-    # @param [Hash] data
-    #   the scenario data
-    #
-    def scenario(data)
-      raise 'You need to define the `scenario` method in your reporter!'
-    end
-
-    # Receives this hook when a step is invoked
-    # @param [Hash] step
-    #   the step data
-    # @param [Symbol] result
-    #   the step name and its finishing state. May be :success or :failure
-    #
-    def step(step, result)
-      raise 'You need to define the `step` method in your reporter!'
-    end
-
-    # Ends with a status
-    def end(status)
-      raise "Abstract method!"
-    end
-
     def bind
-      Runner.after_run method(:_after_run)
-      Runner::Feature.before_run method(:_before_feature_run)
-      Runner::Feature.after_run method(:_after_feature_run)
-      Runner::Scenario.before_run method(:_before_scenario_run)
-      Runner::Scenario.after_run method(:_after_scenario_run)
-      Runner::Scenario.on_successful_step method(:_on_successful_step)
-      Runner::Scenario.on_failed_step method(:_on_failed_step)
-      Runner::Scenario.on_error_step method(:_on_error_step)
-      Runner::Scenario.on_skipped_step method(:_on_skipped_step)
+      Runner.after_run method(:after_run)
+      Runner::Feature.before_run method(:before_feature_run)
+      Runner::Feature.after_run method(:after_feature_run)
+      Runner::Scenario.before_run method(:before_scenario_run)
+      Runner::Scenario.after_run method(:after_scenario_run)
+      Runner::Scenario.on_successful_step method(:on_successful_step)
+      Runner::Scenario.on_failed_step method(:on_failed_step)
+      Runner::Scenario.on_error_step method(:on_error_step)
+      Runner::Scenario.on_skipped_step method(:on_skipped_step)
     end
 
-    def _after_run(status)
-      self.end(status)
-    end
+    def after_run(*args); end;
 
-    def _before_feature_run(data)
-      feature(data)
+    def before_feature_run(data)
       current_feature = data
     end
 
-    def _after_feature_run(data)
+    def after_feature_run(data)
       current_feature = nil
     end
 
-    def _before_scenario_run(data)
-      scenario(data)
+    def before_scenario_run(data)
       current_scenario = data
     end
 
-    def _after_scenario_run(data)
-      after_scenario
+    def after_scenario_run(data)
       current_scenario = nil
     end
 
-    def _on_successful_step(step)
-      step(step, :success)
-    end
-
-    def _on_failed_step(step, failure)
-      step(step, :failure, failure)
-    end
-
-    def _on_error_step(step, failure)
-      step(step, :error, failure)
-    end
-
-    def _on_skipped_step(step)
-      step(step, :skip)
-    end
+    def on_successful_step(step); end;
+    def on_failed_step(step, failure); end;
+    def on_error_step(step, failure); end;
+    def on_undefined_step(step); end;
+    def on_skipped_step(step); end;
 
   end
 end
--- test/spinach/reporter_test.rb
+++ test/spinach/reporter_test.rb
@@ -2,35 +2,4 @@
 require_relative '../test_helper'
 
 describe Spinach::Reporter do
-  describe "abstract methods" do
-    before do
-      @reporter = Spinach::Reporter.new
-    end
-    %w{feature scenario}.each do |abstract_method|
-      describe "#{abstract_method}" do
-        it "raises an error" do
-          Proc.new{
-            @reporter.send(abstract_method, stub_everything)
-          }.must_raise RuntimeError
-        end
-      end
-    end
-
-    describe "#step" do
-      it "raises an error" do
-        Proc.new{
-          @reporter.step(stub_everything, :success)
-        }.must_raise RuntimeError
-      end
-    end
-
-    describe "#end" do
-      it "raises an error" do
-        Proc.new{
-          @reporter.end(0)
-        }.must_raise RuntimeError
-      end
-    end
-  end
-
 end
