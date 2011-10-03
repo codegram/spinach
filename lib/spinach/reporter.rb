bdcd75f902ab94a860090ccc61f4d627500dd0c0
Josep Jaume <josepjaume@gmail.com>
2011-10-03 20:50:38 +0200

    More refactoring

--- lib/spinach/reporter.rb
+++ lib/spinach/reporter.rb
@@ -17,9 +17,9 @@ module Spinach
 
     # A Hash with options for the reporter
     #
-    attr_accessor :options
+    attr_reader :options
 
-    attr_accessor :current_feature, :current_scenario
+    attr_reader :current_feature, :current_scenario
 
     attr_reader :undefined_steps, :failed_steps, :error_steps
 
@@ -33,31 +33,39 @@ module Spinach
       Runner::Scenario.on_failed_step method(:on_failed_step)
       Runner::Scenario.on_error_step method(:on_error_step)
       Runner::Scenario.on_skipped_step method(:on_skipped_step)
+
+      Runner::Feature.before_run method(:current_feature=)
+      Runner::Feature.after_run method(:clear_current_feature)
+      Runner::Scenario.before_run method(:current_scenario=)
+      Runner::Scenario.after_run method(:clear_current_scenario)
     end
 
     def after_run(*args); end;
-
-    def before_feature_run(data)
-      current_feature = data
-    end
-
-    def after_feature_run(data)
-      current_feature = nil
-    end
-
-    def before_scenario_run(data)
-      current_scenario = data
-    end
-
-    def after_scenario_run(data)
-      current_scenario = nil
-    end
-
+    def before_feature_run(data); end
+    def after_feature_run(data); end
+    def before_scenario_run(data); end
+    def after_scenario_run(data); end
     def on_successful_step(step); end;
     def on_failed_step(step, failure); end;
     def on_error_step(step, failure); end;
     def on_undefined_step(step); end;
     def on_skipped_step(step); end;
+
+    def clear_current_feature(*args)
+      current_feature = nil
+    end
+
+    def current_feature=(data)
+      current_feature = data
+    end
+
+    def current_scenario=(data)
+      current_scenario = data
+    end
+
+    def clear_current_scenario(*args)
+      current_scenario = nil
+    end
 
   end
 end
