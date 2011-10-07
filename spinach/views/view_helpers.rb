31025e750c099c482826c9ea61bb8b569b7b7dea
Josep Jaume <josepjaume@gmail.com>
2011-10-07 17:15:22 +0200

    Add pages

--- spinach/views/view_helpers.rb
+++ spinach/views/view_helpers.rb
@@ -4,22 +4,4 @@
 # views pristine.
 #
 module ViewHelpers
-  
-  # Calculate the years for a copyright
-  def copyright_years(start_year)
-    end_year = Date.today.year
-    if start_year == end_year
-      "\#{start_year}"
-    else
-      "\#{start_year}&#8211;\#{end_year}"
-    end
-  end
-  
-  # Handy for hiding a block of unfinished code
-  def hidden(&block)
-    #no-op
-  end
-  
-  # Add your own helpers below...
-  
 end
