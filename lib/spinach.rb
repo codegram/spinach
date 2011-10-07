57378a48d341df7ac5d03fa8161045ed351521af
Josep M. Bach <josep.m.bach@gmail.com>
2011-10-07 16:21:18 +0200

    document spinach module

--- lib/spinach.rb
+++ lib/spinach.rb
@@ -9,35 +9,43 @@ require_relative 'spinach/feature'
 require_relative 'spinach/reporter'
 require_relative 'spinach/cli'
 
-
-# Spinach is a BDD framework in top of gherkin. Its main goals are:
-#   * No magic: All features are implemented using actual classes.
-#   * Reusability: Steps are methods, so they can be put inside modules.
+# Spinach is a BDD framework leveraging the great Gherkin language. This
+# language is the one used defining features in Cucumber, the BDD framework
+# Spinach is inspired upon.
+#
+# Its main design goals are:
+#
+#   * No magic: All features are implemented using normal Ruby classes.
+#   * Reusability: Steps are methods, so they can be reused using modules, a
+#     common, normal practice among rubyists.
 #   * Proper encapsulation: No conflicts between steps from different
 #     scenarios.
 #
 module Spinach
   @@features = []
 
-  # @return [Array<Spinach::Feature>]
-  #   all the registered features
+  # @return [Array<Feature>]
+  #   All the registered features.
   #
+  # @api public
   def self.features
     @@features
   end
 
-  # Resets Spinach to a pristine state, as if any feature was registered.
+  # Resets Spinach to a pristine state, as if no feature was ever registered.
   # Mostly useful in Spinach's own testing.
   #
+  # @api semipublic
   def self.reset_features
     @@features = []
   end
 
-  # Finds a feature given a feature name
+  # Finds a feature given a feature name.
   #
   # @param [String] name
-  #   the feature name
+  #   The feature name.
   #
+  # @api public
   def self.find_feature(name)
     klass = Spinach::Support.camelize(name)
     @@features.detect do |feature|
