016c70f7e7b31cf6c5b18b9e2bf49905b912378c
Josep M. Bach <josep.m.bach@gmail.com>
2011-10-07 15:44:23 +0200

    document parser

--- lib/spinach/parser.rb
+++ lib/spinach/parser.rb
@@ -2,28 +2,35 @@ require 'gherkin'
 require 'gherkin/formatter/json_formatter'
 
 module Spinach
-  # Spinach's parser uses Gherkin in the guts to extract all the information
-  # needed from the plain feature definition.
+  # Parser leverages Gherkin to parse the feature definition.
   #
   class Parser
     # @param [String] filename
-    #   the file to parse
+    #   The filename to parse.
     #
+    # @api public
     def initialize(filename)
       @filename = filename
       @formatter = Gherkin::Formatter::JSONFormatter.new(nil)
       @parser = Gherkin::Parser::Parser.new(@formatter)
     end
 
-    # Gets the plain text content of the feature file.
-    # @return [String]
-    #   the plain feature content
+    # Gets the plain text content out of the feature file.
     #
+    # @return [String]
+    #   The plain feature content.
+    #
+    # @api public
     def content
       File.read(@filename)
     end
 
-    # @return [Hash] the parsed gerkin output
+    # Parses the feature file and returns an AST as a Hash.
+    #
+    # @return [Hash]
+    #   The parsed Gherkin output.
+    #
+    # @api public
     def parse
       @parser.parse(content, @filename, __LINE__-1)
       @formatter.gherkin_object
