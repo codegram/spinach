658a95e2bbf97786e36785952096867c3799300a
Josep Jaume <josepjaume@gmail.com>
2011-10-09 18:02:24 +0200

    Change test syntax

--- test/spinach/config_test.rb
+++ test/spinach/config_test.rb
@@ -1,45 +1,45 @@
 require_relative '../test_helper'
 
 describe Spinach::Config do
-  before do
-    @config = Spinach::Config.new
+  subject do
+    Spinach::Config.new
   end
 
   describe '#step_definitions_path' do
     it 'returns a default' do
-      @config[:step_definitions_path].must_be_kind_of String
+      subject[:step_definitions_path].must_be_kind_of String
     end
 
     it 'can be overwritten' do
-      @config[:step_definitions_path] = 'steps'
-      @config[:step_definitions_path].must_equal 'steps'
+      subject[:step_definitions_path] = 'steps'
+      subject[:step_definitions_path].must_equal 'steps'
     end
   end
 
   describe '#support_path' do
     it 'returns a default' do
-      @config[:support_path].must_be_kind_of String
+      subject[:support_path].must_be_kind_of String
     end
 
     it 'can be overwritten' do
-      @config[:support_path] = 'support'
-      @config[:support_path].must_equal 'support'
+      subject[:support_path] = 'support'
+      subject[:support_path].must_equal 'support'
     end
   end
 
   describe '#failure_exceptions' do
     it 'returns a default' do
-      @config[:failure_exceptions].must_be_kind_of Array
+      subject[:failure_exceptions].must_be_kind_of Array
     end
 
     it 'can be overwritten' do
-      @config[:failure_exceptions] = [1, 2, 3]
-      @config[:failure_exceptions].must_equal [1,2,3]
+      subject[:failure_exceptions] = [1, 2, 3]
+      subject[:failure_exceptions].must_equal [1,2,3]
     end
 
     it 'allows adding elements' do
-      @config[:failure_exceptions] << RuntimeError
-      @config[:failure_exceptions].must_include RuntimeError
+      subject[:failure_exceptions] << RuntimeError
+      subject[:failure_exceptions].must_include RuntimeError
     end
   end
 end
