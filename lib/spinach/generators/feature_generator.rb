da2bc67eac914bfe0146578fda0181dc3ae8a2a5
Josep Jaume <josepjaume@gmail.com>
2011-10-10 23:29:22 +0200

    Wip

--- lib/spinach/generators/feature_generator.rb
+++ lib/spinach/generators/feature_generator.rb
@@ -1,4 +1,16 @@
 module Spinach
   class Generators::FeatureGenerator
+    attr_reader :data
+
+    def initialize(data)
+      @data = data
+    end
+
+    def steps
+    end
+
+    def generate
+    end
+
   end
 end
--- test/spinach/generators/feature_generator_test.rb
+++ test/spinach/generators/feature_generator_test.rb
@@ -4,7 +4,24 @@ require_relative '../../../lib/spinach/generators'
 module Spinach::Generators
   describe FeatureGenerator do
     subject do
-      FeatureGenerator.new
+      FeatureGenerator.new(data)
+    end
+    let(:data) do
+      Spinach::Parser.new("
+                          Feature: Funky stuff
+                            Scenario: Some Lulz
+                              Given I haz a sad
+                              When I get some lulz
+                              Then I has a happy
+                            Scenario: Some sad
+                              Given I haz a happy
+                              When I get some lulz
+                              Then I am OMG ROFLMAO
+                          ").parse
+    end
+    describe "#steps" do
+      it "returns the different steps for this data" do
+      end
     end
   end
 end
