f0d0a150dad39d851ab17a90008713da916f8c5b
Josep Jaume <josepjaume@gmail.com>
2011-10-10 23:10:54 +0200

    Step generator

--- lib/spinach/generators/step_generator.rb
+++ lib/spinach/generators/step_generator.rb
@@ -1,4 +1,20 @@
 module Spinach
   class Generators::StepGenerator
+    attr_reader :data
+
+    def initialize(data)
+      @data = data
+    end
+
+    def generate
+      result = StringIO.new
+      result.puts "#{data['keyword']} '#{escape data['name']}' do"
+      result.puts "end"
+      result.string
+    end
+
+    def escape(text)
+      text.sub("'", "\\\\'")
+    end
   end
 end
--- test/spinach/generators/step_generator_test.rb
+++ test/spinach/generators/step_generator_test.rb
@@ -1,10 +1,28 @@
 require_relative '../../test_helper'
-require_relative '../../../lib/spinach/generators/step_generator'
+require_relative '../../../lib/spinach/generators'
 
 module Spinach::Generators
   describe StepGenerator do
     subject do
-      StepGenerator.new
+      StepGenerator.new(data)
+    end
+
+    let(:data) do
+      {'keyword' => 'Given', 'name' => "I has a sad"}
+    end
+
+    describe "#generate" do
+      it "generates a step" do
+        subject.generate.must_match /Given.*I has a sad/
+      end
+    end
+
+    describe "#escape" do
+      it "escapes the name" do
+        subject.escape(
+          "I've been doing bad things"
+        ).must_include "I\\'ve been doing bad things"
+      end
     end
   end
 end
