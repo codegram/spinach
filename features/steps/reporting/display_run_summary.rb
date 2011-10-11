605468d9ccf1c2a36a4c0f1e59da95b5774a263c
Marc Divins <marcdivc@gmail.com>
2011-10-11 12:20:27 +0200

    Finish display_run_summary feature

--- features/steps/reporting/display_run_summary.rb
+++ features/steps/reporting/display_run_summary.rb
@@ -12,16 +12,16 @@ Feature "Display run summary" do
         When I jump from Codegrams roof
         Then I must be pwned by floor
 
-      # Scenario: Failed scenario
-      #   Given I love risk
-      #   When I jump from Codegrams roof
-      #   Then my parachute must open
-      #   Then I must not pwned by floor
+      Scenario: Failed scenario
+        Given I love risk
+        When I jump from Codegrams roof
+        Then my parachute must open
+        Then I must not be pwned by floor
 
-      # Scenario: Error scenario
-      #   Given I am not a fool
-      #   When I go downstairs
-      #   Then I must succeed
+      Scenario: Error scenario
+        Given I am not a fool
+        When I go downstairs
+        Then I must succeed
      ')
     write_file('features/steps/test_feature.rb',
     'Feature "A test feature" do
@@ -29,21 +29,21 @@ Feature "Display run summary" do
       end
       When "I jump from Codegrams roof" do
       end
-      # Given "I love risk" do
-      # end
-      # And "my parachute must open" do
-      #   false
-      # end
-      # Given I am a fool
-      #   djkadlaksaldkajd
-      # end
-      # Given I am not a fool
-      # end
-      # When I go downstairs
-      # end
-      # Then I must succeed
-      #   true
-      # end
+      Given "I love risk" do
+      end
+      And "my parachute must open" do
+        false.must_equal true
+      end
+      Given "I am a fool" do
+      end
+      Given "I am not a fool" do
+        adaksjdald
+      end
+      When "I go downstairs" do
+      end
+      Then "I must succeed" do
+        true
+      end
      end')
     @feature = "features/test_feature.feature"
   end
@@ -54,7 +54,7 @@ Feature "Display run summary" do
 
   Then "I should see a summary with steps status information" do
     all_stdout.must_match(
-      /Summary:.*2.*Successful.*1.*Undefined.*0.*Failed.*0.*Error/
+      /Summary:.*4.*Successful.*1.*Undefined.*1.*Failed.*1.*Error/
     )
   end
 end
