dfeace4ca1b7ac1621719ee72c105412770870eb
Yi Wen <ywen.github@gmail.com>
2012-08-13 09:19:45 -0500

    Refactoring
    Make the inheritance feature not dependent on another test

--- features/steps/before_and_after_hooks.rb
+++ features/steps/before_and_after_hooks.rb
@@ -1,15 +1,9 @@
 class BeforeAndAfterHooks < Spinach::FeatureSteps
   class << self
     attr_accessor :var1
-    attr_accessor :var2
   end
 
   before do
-    if self.class.var2.nil?
-      self.class.var2 = :i_am_here
-    else
-      self.class.var2 = :dirty
-    end
     if self.class.var1.nil?
       self.class.var1 = :clean
     else
--- features/steps/before_and_after_hooks_inheritance.rb
+++ features/steps/before_and_after_hooks_inheritance.rb
@@ -1,5 +1,29 @@
-require_relative "before_and_after_hooks"
-class BeforeAndAfterHooksInheritance < BeforeAndAfterHooks
+class BeforeAndAfterHooksInheritanceBase < Spinach::FeatureSteps
+  class << self
+    attr_accessor :var1
+    attr_accessor :var2
+  end
+
+  before do
+    if self.class.var2.nil?
+      self.class.var2 = :i_am_here
+    else
+      self.class.var2 = :dirty
+    end
+    if self.class.var1.nil?
+      self.class.var1 = :clean
+    else
+      self.class.var1 = :dirty
+    end
+  end
+
+  after do
+    self.class.var1 = nil
+  end
+
+end
+
+class BeforeAndAfterHooksInheritance < BeforeAndAfterHooksInheritanceBase
   before do
     self.class.var1 = :in_subclass
   end
