7cb55c92a006d0bddbca81c5c8c24a4b801b5c29
Oriol Gual <oriol.gual@gmail.com>
2012-12-18 09:38:57 +0100

    Generate steps with 'step' instead of Given, When, Then.
    Closes #100.

--- test/spinach/generators/feature_generator_test.rb
+++ test/spinach/generators/feature_generator_test.rb
@@ -37,8 +37,8 @@ Feature: Cheezburger can I has
     describe "#generate" do
       it "generates an entire feature_steps class definition" do
         result = subject.generate
-        result.must_match /Given 'I haz a sad' do/
-        result.must_match /pending 'step not implemented'/
+        result.must_match(/step 'I haz a sad' do/)
+        result.must_match(/pending 'step not implemented'/)
       end
     end
 
