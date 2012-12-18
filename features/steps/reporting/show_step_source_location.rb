0642a969a233fad5f2d7c6ecd5b9cec091266f9c
Oriol Gual <oriol.gual@gmail.com>
2012-12-18 15:36:25 +0100

    Fix Travis error.

--- features/steps/reporting/show_step_source_location.rb
+++ features/steps/reporting/show_step_source_location.rb
@@ -1,10 +1,7 @@
-class ShowStepSourceLocation < Spinach::FeatureSteps
-
-  feature "Show step source location"
-
+class Spinach::Features::ShowStepSourceLocation < Spinach::FeatureSteps
   include Integration::SpinachRunner
 
-  Given "I have a feature that has no error or failure" do
+  step "I have a feature that has no error or failure" do
     write_file('features/success_feature.feature', """
 Feature: A success feature
 
@@ -13,25 +10,25 @@ Feature: A success feature
 """)
 
     write_file('features/steps/success_feature.rb',
-    'class ASuccessFeature < Spinach::FeatureSteps
+    'class Spinach::Features::ASuccessFeature < Spinach::FeatureSteps
       feature "A success feature"
-      Then "I succeed" do
+      step "I succeed" do
       end
      end')
     @feature = "features/success_feature.feature"
   end
 
-  When "I run it" do
+  step "I run it" do
     run_feature @feature
   end
 
-  Then "I should see the source location of each step of every scenario" do
+  step "I should see the source location of each step of every scenario" do
     @stdout.must_match(
       /I succeed.*features\/steps\/success_feature\.rb.*3/
     )
   end
 
-  Given "I have a feature that has no error or failure and use external steps" do
+  step "I have a feature that has no error or failure and use external steps" do
     write_file('features/success_feature.feature', """
 Feature: A feature that uses external steps
 
@@ -40,26 +37,26 @@ Feature: A feature that uses external steps
 """)
 
     write_file('features/steps/success_feature.rb',
-    'class AFeatureThatUsesExternalSteps < Spinach::FeatureSteps
+    'class Spinach::Features::AFeatureThatUsesExternalSteps < Spinach::FeatureSteps
       feature "A feature that uses external steps"
       include ExternalSteps
      end')
     write_file('features/support/external_steps.rb',
     'module ExternalSteps
       include Spinach::DSL
-      Given "this is a external step" do
+      step "this is a external step" do
       end
     end')
     @feature = "features/success_feature.feature"
   end
 
-  Then "I should see the source location of each step, even external ones" do
+  step "I should see the source location of each step, even external ones" do
     @stdout.must_match(
       /this is a external step.*features\/support\/external_steps\.rb.*3/
     )
   end
 
-  Given "I have a feature that has an error" do
+  step "I have a feature that has an error" do
     write_file('features/error_feature.feature', """
 Feature: An error feature
 
@@ -68,22 +65,22 @@ Feature: An error feature
 """)
 
     write_file('features/steps/error_feature.rb',
-    'class AnErrorFeature < Spinach::FeatureSteps
+    'class Spinach::Features::AnErrorFeature < Spinach::FeatureSteps
       feature "An error feature"
-      Then "I do not succeed" do
+      step "I do not succeed" do
         i_do_not_exist.must_be_equal "Your Mumma"
       end
      end')
     @feature = "features/error_feature.feature"
   end
 
-  Then "I should see the source location of each step, even ones with errors" do
+  step "I should see the source location of each step, even ones with errors" do
     @stdout.must_match(
       /I do not succeed.*features\/steps\/error_feature\.rb.*3/
     )
   end
 
-  Given "I have a feature that has a failure" do
+  step "I have a feature that has a failure" do
     write_file('features/failure_feature.feature', """
 Feature: A failure feature
 
@@ -92,9 +89,9 @@ Feature: A failure feature
 """)
 
     write_file('features/steps/failure_feature.rb',
-    'class AFailureFeature < Spinach::FeatureSteps
+    'class Spinach::Features::AFailureFeature < Spinach::FeatureSteps
       feature "A failure feature"
-      Then "I do not succeed" do
+      step "I do not succeed" do
         i_exist = "Your Pappa"
         i_exist.must_be_equal "Your Mumma"
       end
@@ -102,7 +99,7 @@ Feature: A failure feature
     @feature = "features/failure_feature.feature"
   end
 
-  Then "I should see the source location of each step, even ones with failures" do
+  step "I should see the source location of each step, even ones with failures" do
     @stdout.must_match(
       /I do not succeed.*features\/steps\/failure_feature\.rb.*3/
     )
